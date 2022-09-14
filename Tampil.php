fb061da5619f2d349e6f930f5547de55077cedc2
qmak4398 <85492366+muhagand@users.noreply.github.com>
2022-09-14 17:36:01 +0700

    Update Tampil.php

--- Tampil.php
+++ Tampil.php
@@ -1,1 +1,31 @@
-1 <!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Hasil Login</title>
+  <link rel="styesheet" href="style.css" type="text/css">
+  </head>
+  <body>
+    
+<?php
+$nama = $_POST["username"];
+$email = $_POST["email"];
+    
+?>
+<from action="tampil.php" method="POST">
+  <h2>Selamat Anda berhasil login </h2>
+  <div class="omgcontainer">
+    <img scr="icon.png" alt="Avatar" class="avatar">
+  </div>
+  
+  <div class="container">
+    <label><b>Username : <?php echo $nama:?></b></label><br><br>
+    
+    <label><b>Password : <?php echo $email:?></b></label></div>
+  
+  <div class="container" style="background-color:#f1f1f1">
+    
+    </div>
+</form>
+    
+</body>
+</html>  
